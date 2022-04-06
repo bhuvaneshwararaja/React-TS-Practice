17abca0a407aefeba23c384899ca6967c7267d18
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2022-04-06 14:23:26 +0530

    Rendering component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import InputTaskField from './components/InputFiled'
+import TaskList from './components/Tasklist';
+import {TodoList} from "./model"
 function App() {
+
+
+  const [todo,setTodo] = useState<string>("")
+
+  const [todos,setTodos] = useState<TodoList[]>([])
+
+  function  handleAdd(e:React.FormEvent){
+      e.preventDefault()
+      setTodos([...todos,{id:Date.now(),todo,isDone:false}])
+      setTodo("")
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <h1>Taskify</h1>
+     <InputTaskField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+     <div className="container">
+        <TaskList todos={todos} setTodos={setTodos} todo={todo} setTodo={setTodo} />
+     </div>
+   </div>
   );
 }
 
